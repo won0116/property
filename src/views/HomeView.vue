4065494ecb5ad530ed2a732153aec3cb6cd0f3d8
유종원 <todidas@naver.com>
2022-09-27 13:45:18 +0900

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
+  mounted() {
+    this.fetchData();
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    fetchData: function () {
+      axios
+        .get(
+          "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?serviceKey=v4tAgOkXnwC28Mrxb6RplngbHziwshZRKMeUnGqdDagccrp%2BXa%2Fqoy%2BqYuxJQxBaGXN%2BJeX5iSWl5eKV%2BlDKrA%3D%3D&LAWD_CD=11110&DEAL_YMD=201512"
+        )
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
